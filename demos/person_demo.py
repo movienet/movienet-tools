c5de35c467c14dde2ccd105e4188502dc69b6e27
hqqasw <hqqasw@qq.com>
2020-06-17 16:18:55 +0800

    fix bug in person demo

--- demos/person_demo.py
+++ demos/person_demo.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     detector = PersonDetector('rcnn', cfg, weight)
     print('build model done: {:.2f}s'.format(time.time() - st))
 
-    img_path = './tests/data/still02.jpg'
+    img_path = './tests/data/still01.jpg'
     img = cv2.imread(img_path)
     persons = detector.detect(img, show=True, conf_thr=0.5)
     assert persons.shape[0] == 2
